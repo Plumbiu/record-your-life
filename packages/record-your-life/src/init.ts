24ffea0ebcdfd74c1b2065a5e0cf373e3b21d54d
Plumbiu <3434909403@qq.com>
2024-01-09 20:00:01 +0800

    fix: bug

--- packages/record-your-life/src/init.ts
+++ packages/record-your-life/src/init.ts
@@ -8,7 +8,7 @@ import { findApp, getInstalledApps } from './utils'
 
 const records: Map<string, Usage> = new Map()
 
-function insertRecord(name: string | undefined, shouldUpdate = false) {
+function insertRecord(name: string | undefined) {
   if (!name) {
     return
   }
@@ -22,7 +22,7 @@ function insertRecord(name: string | undefined, shouldUpdate = false) {
       start: now,
       durations: [],
     })
-  } else if (shouldUpdate) {
+  } else {
     record.durations.push({ time: now, duration: record.total })
     record.total += now - record.end
     record.end = now
@@ -47,20 +47,26 @@ export async function init(timer: number, config: Config) {
   let shoudWrite = false
   let count = 0
   const apps = await getInstalledApps()
-  let unkownPreApp: string | undefined
   WatchWindowForeground(async (_curr, prevId, win) => {
     const preApp = findApp(apps, getHandleProcessID(prevId))
     const curApp = findApp(apps, win.pid)
-    insertRecord(preApp ?? unkownPreApp, true)
-    insertRecord(curApp ?? win.title)
-    unkownPreApp = win.title
+    insertRecord(preApp)
     if (curApp) {
-      shoudWrite = true
+      insertRecord(curApp)
+    } else {
+      if (win.title) {
+        console.log(win.title)
+
+        insertRecord(win.title)
+      }
     }
+    shoudWrite = true
   })
   setInterval(async () => {
     count++
     if (shoudWrite || count > 6) {
+      console.log(111)
+
       await fsp.writeFile(
         todayFile,
         JSON.stringify(Object.fromEntries(records)),
