a48d571d7be53271ed2941cb41dfd8513dd7e7ed
Plumbiu <3434909403@qq.com>
2024-01-11 11:06:22 +0800

    chore: build option

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,18 +1,18 @@
-import { execSync } from 'node:child_process'
-import { mkdirSync, writeFileSync } from 'node:fs'
+import { exec, execSync } from 'node:child_process'
+import { writeFile, mkdir } from 'node:fs/promises'
 
 execSync('pnpm -F record-your-life run build')
 
-try {
-  writeFileSync(
-    './packages/record-your-life/dist/record-your-life.json',
-    JSON.stringify({
-      storagePath: './storage',
-    }),
-  )
-  mkdirSync('./packages/record-your-life/dist/storage')
-} catch (error: any) {
-  console.log(error.message)
+async function initConfig() {
+  try {
+    await writeFile(
+      './packages/record-your-life/dist/record-your-life.json',
+      JSON.stringify({
+        storagePath: './storage',
+      }),
+    )
+    await mkdir('./packages/record-your-life/dist/storage')
+  } catch (error) {}
 }
 
-execSync('pnpm -F web run build')
+Promise.all([initConfig(), exec('pnpm -F web run build')])
