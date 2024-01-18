6c349de57a02e706c6d39804d91f8a3411159655
Plumbiu <3434909403@qq.com>
2024-01-18 20:44:58 +0800

    fix: log build

--- scripts/constant.ts
+++ scripts/constant.ts
@@ -1,4 +1,8 @@
 import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+export const __dirname = path.dirname(__filename)
 
 export const RECORD_YOUR_LIFE = './packages/record-your-life/'
 export const RECORD_YOUR_LIFE_DIST = path.join(RECORD_YOUR_LIFE, 'dist')
--- scripts/log.ts
+++ scripts/log.ts
@@ -1,13 +1,13 @@
 import { exec } from 'node:child_process'
 import path from 'node:path'
-import { RECORD_YOUR_LIFE } from './constant'
+import { RECORD_YOUR_LIFE, __dirname } from './constant'
 
 const args = ['--list', '--table', '--bar', '--board']
 const cmd = `esno ${path.join(
   RECORD_YOUR_LIFE,
   'src',
   'index.ts',
-)} --file-name ./scripts/data-log.json `
+)} --file-name ${path.join(__dirname, 'data-log.json')} `
 
 args.map(async (arg) => {
   exec(cmd + arg, (err, data) => {
